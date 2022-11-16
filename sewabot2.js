97533e0733f41671d302b74bf4f55de6a535a3bb
Hekallalala <115947993+Hekallalala@users.noreply.github.com>
2022-11-16 12:10:06 +0700

    Add files via upload

---
+++ sewabot2.js
@@ -0,0 +1,42 @@
+let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
+let url = 'https://telegra.ph/file/c744e9d823ae3db3fd159.jpg'
+let text = `━━ꕥ〔 *𝚂𝙴𝚆𝙰 & 𝙿𝚁𝙴𝙼𝙸𝚄𝙼* 〕ꕥ━⬣
+
+┏━ꕥ〔 𝙷𝙰𝚁𝙶𝙰 𝚂𝙴𝚆𝙰 〕
+┃
+┃✾ 1 MINGU 5K / ɢʀᴏᴜᴘ
+┃✾ 1 BULAN 10K / ɢʀᴏᴜᴘ
+┃✾ PREMIUM 5K / ᴜꜱᴇʀ
+┃
+┗━━ꕥ *${author}* ꕥ━⬣
+
+┏━ꕥ〔 ꜰɪᴛᴜʀ 450+ 〕
+┃
+┃✾ ᴡᴇʟᴄᴏᴍᴇ
+┃✾ ᴋɪᴄᴋ
+┃✾ ᴀɴᴛɪʟɪɴᴋ
+┃✾ ꜱᴛɪᴋᴇʀ
+┃✾ ꜱᴏᴜɴᴅ
+┃✾ ᴀɴɪᴍᴇ
+┃✾ ꜱᴇɴᴅ ᴠɪʀᴛᴇx
+┃✾ ꜱᴇɴᴅ ʙᴜɢ
+┃✾ ɢᴀᴍᴇ ʀᴘɢ
+┃✾ ᴅʟʟ
+┃
+┗━━ꕥ *${author}* ꕥ━⬣
+`
+const templateButtons = [
+    {index: 1, urlButton: {displayText: 'Klick untuk Sewa', url: `https://wa.me/6285730794089?text=Hallo admin, saya tertarik untuk menyewa bot ${namebot}`}},
+    {index: 4, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
+]
+let tm = {
+text: text,
+footer: global.wm,
+templateButtons: templateButtons,
+image: url
+}
+conn.sendMessage(m.chat, tm, m)
+}
+handler.command = /^(masuk)$/i
+
+export default handler
